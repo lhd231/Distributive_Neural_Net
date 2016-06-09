bd25529bf2c664aee771ce40fa7481738d5027dc
lhd231 <lhd231@gmail.com>
2016-06-09 15:21:52 -0600

    Updated code
    This code runs 3 local sites, a pooled site, and our distributed program.  The data is separated into three sections (two sets of horns, and middle points).  The plot code plots the text files produced from the run_code

---
+++ plot.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 16 16:26:09 2016
+
+@author: LHD
+"""
+#import pylab as plt
+import numpy as np
+import seaborn
+import pandas as pd
+#plt.xlabel("Number of batches [of size 50]")
+#plt.ylabel("Error rate")
+c4 = np.loadtxt("classic4-trunc.txt")
+c3 = np.loadtxt("classAcc3-trunc.txt")
+c2 = np.loadtxt("classAcc2-trunc.txt")
+c1 = np.loadtxt("classAcc1-trunc.txt")
+dif = np.loadtxt("nn1Acc-trunc.txt")
+class2 = np.mean(c2, axis=0)
+print class2[0]
+class3 = np.mean(c3, axis=0)
+class1 = np.mean(c1, axis=0)
+differ = np.mean(dif,axis=0)
+class4 = np.mean(c4, axis=0)
+#plt.plot(nn1Acc[0], "-r", label = "differential")
+#plt.plot(nn1Acc[1], "-r", label = "differential")
+#plt.plot(class2, "-b", label = "classic 1")
+errHigh = class2 + np.std(c2,axis=0)
+error = class3 + np.std(c3,axis=0)
+errLow = differ + np.std(dif,axis=0)
+errup = class1 + np.std(c1,axis=0)
+errDown = class4 + np.std(c4,axis=0)
+x = np.arange(len(class2))
+#plt.errorbar(x,class2,yerr=errHigh, label="Classic 1")
+#plt.errorbar(x,differ,yerr=errLow, label="differential")
+#plt.errorbar(x,class3,yerr=error, label="Classic Combined")
+#plt.errorbar(x,class1,yerr=errup, label="Classic 1")
+axe = np.array(["diff" for _ in range(10)])
+
+#print seaborn.load_dataset("tips")
+nw = np.c_[dif,axe]
+axe = np.array(["c3" for _ in range(10)])
+cw = np.c_[c3,axe]
+axe = np.array(["c1" for _ in range(10)])
+c1w = np.c_[c1,axe]
+axe = np.array(["c2" for _ in range(10)])
+c2w = np.c_[c2,axe]
+axe = np.array(["cc" for _ in range(10)])
+c3w = np.c_[c4,axe]
+fw = np.concatenate((nw, cw), axis=0)
+fw = np.concatenate((fw,c1w), axis=0)
+#arrayfire
+fw = np.concatenate((fw,c2w), axis=0)
+fw = np.concatenate((fw,c3w), axis=0)
+s = pd.DataFrame(fw, columns=[10,20,40,60,80,"class"])
+#seaborn.boxplot(data = fw, hue_order="class")
+#print s
+df_long = pd.melt(s, "class", var_name="sample size", value_name="error rate")
+#print df_long
+
+#seaborn.factorplot(data=s, kind="box")
+seaborn.factorplot(x="sample size", y="error rate", hue="class",data=df_long, kind="box")
+seaborn.plt.show()
+#seaborn.factorplot("sample size", hue="class", y="error rate", data=df_long, kind="box")
+#seaborn.boxplot(data=dif)
+#seaborn.boxplot(data=c3)
+#seaborn.boxplot(class2)
+#plt.plot(class2)
+#plt.errorbar(c2[0],c2[1],c2[2],c2[3],linestyle='None', marker='^')
+#plt.plot(classAcc1, "-g", label = "classic 2")
+#plt.plot(classAcc3, "-p", label = "Classic Combined")
+#plt.legend(loc='upper right')
---
+++ run_code.py
@@ -0,0 +1,248 @@
+import numpy as np
+#from nn import nn_build, forward, master_node, plot_decision2D
+import nn as nnDif
+from sklearn.cross_validation import train_test_split
+from sklearn.datasets import make_moons
+from numpy import count_nonzero
+import time
+import pylab as plt
+import nnS
+import math
+import random
+from scipy.stats import multivariate_normal as norm
+
+minibatch = 2
+nonlin = 'relu'
+eta = 0.0025
+
+def gauss(x,mu,sigma):
+  
+#  y = 1 / (math.sqrt(2.0 * math.pi * sigma) * (math.exp(((x[0] - mu[0] + 0.0) / sigma **2) + ((x[1] - mu[1] + 0.0) / sigma **2))))
+  return norm(mu,[[sigma,0],[0, sigma]]).pdf(x)
+
+
+def split(data, label):
+  #plt.scatter(data[:,0],data[:,1], c=label)
+  
+  print np.var(data)
+  pointMid1 = (0,1)
+  pointMid2 = (1,-.5)
+  pointEdge1 = (-.9,.4)
+  pointEdge2 = (0,.4)
+  pointEdge3 = (1,.5)
+  pointEdge4 = (2,.4)
+  brown1 = list()
+  brown2 = list()
+  green1 = list()
+  green2 = list()
+  red1 = list()
+  red2 = list()
+  brown1Label = list()
+  brown2Label = list()
+  green1Label = list()
+  green2Label = list()
+  red1Label = list()
+  red2Label = list()
+  max = 0
+  i = 0
+  for x,y in zip(data,label):
+    b1 = gauss(x,pointMid1,.1)
+    b2 = gauss(x,pointMid2,.1)
+    g1 = gauss(x,pointEdge1,.1)
+    g2 = gauss(x,pointEdge2,.1)
+    r1 = gauss(x,pointEdge3,.1)
+    r2 = gauss(x,pointEdge4,.1)
+    a = random.random()
+    if y==0 and a < g1:
+      green1.append(x)
+      green1Label.append(y)
+    elif y==1 and a < g2:
+      green2.append(x)
+      green2Label.append(y)
+    elif y==0 and a< b1:
+      brown1.append(x)
+      brown1Label.append(y)
+    elif y==1 and a < b2:
+      brown2.append(x)
+      brown2Label.append(y)
+
+    elif y==0 and a < r1:
+      red1.append(x)
+      red1Label.append(y)
+    elif y==1 and a < r2:
+      red2.append(x)
+      red2Label.append(y)
+    i += 1
+  return_list_data = list()
+  return_list_label = list()
+  return_list_data.append(brown1 + brown2) 
+  return_list_data.append(green1 + green2) 
+  return_list_data.append(red1 + red2)
+  return_list_label.append(brown1Label + brown2Label) 
+  return_list_label.append(green1Label + green2Label)
+  return_list_label.append(red1Label + red2Label)
+  return return_list_data, return_list_label
+
+
+#adagrad and adadelta  esp adadelta
+def iter_minibatches(chunksize, data, labels):
+    # Provide chunks one by one
+    chunkstartmarker = 0
+    numsamples = data.shape[1]
+    while chunkstartmarker < numsamples:
+        chunkrows = range(chunkstartmarker,chunkstartmarker+chunksize)
+        X_chunk, y_chunk = data[:,chunkrows], labels[chunkrows]
+        yield X_chunk, y_chunk
+        chunkstartmarker += chunksize
+
+
+def visitbatches(nn, batches, errlist, it=1000):
+    for c in range(it):
+        for i in range(len(batches)):
+            batch = batches[i]
+            cc = np.mod(c,len(batch))
+            nnDif.master_node(nn, batch[cc][0], batch[cc][1])
+            #err.append(r)
+
+def visitClassicBatches(nn,data, it=1000):
+    for c in range(it):
+        cc = np.mod(c,len(data));
+        nnS.minibatch_fit(nn, data[cc][0], data[cc][1])
+
+def accuracy(nn, data, label, thr = 0.5):
+    predict  = [ np.int8(nnDif.forward(nn,data[c,:]) > thr) == label[c] for c in range(data.shape[0])]
+    return 100 * np.double(len(np.where(np.asarray(predict)==False)[0]))/np.double(len(predict))
+def accuracyClassic(nn, data, label, thr = 0.5):
+    predict  = [ np.int8(nnS.forward(nn,data[c,:]) > thr) == label[c] for c in range(data.shape[0])]
+    return 100 * np.double(len(np.where(np.asarray(predict)==False)[0]))/np.double(len(predict))
+
+def group_list(l, group_size):
+    for i in xrange(0, len(l), group_size):
+        yield l[i:i+group_size]
+nn1Acc = [[0 for i in range(17)] for j in range(10)]
+classAcc1 = [[0 for i in range(17)] for j in range(10)]
+classAcc2 = [[0 for i in range(17)] for j in range(10)]
+classAcc3 = [[0 for i in range(17)] for j in range(10)]
+classAcc4 = [[0 for i in range(17)] for j in range(10)]
+print len(nn1Acc)
+print "len"
+print len(nn1Acc[0])   
+number_of_nets = 3
+for te in range(10):
+    print te
+    data, label = make_moons(n_samples=1500, shuffle=True, noise=0.2,random_state = int(time.time()))
+    print data[0]
+    data,validation_data,label,validation_label = train_test_split(data,label,train_size = .32)
+        #separate the data set into buckets
+    total_data, total_label = split(data,label)
+
+     
+    #total_data = list(group_list(data,10))
+    #total_label = list(group_list(label,10))
+    #The two separate site sets
+    #nn1_groups_data = total_data[:len(total_data)/2+1]
+    print len(total_data[0])
+    print len(total_data[1])
+    nn1_groups_data = list(group_list(total_data[0],10))
+    nn1_groups_label = list(group_list(total_label[0],10))
+    nn2_groups_data = list(group_list(total_data[1],10))
+    nn2_groups_label = list(group_list(total_label[1],10))
+    nn3_groups_data = list(group_list(total_data[2],10))
+    nn3_groups_label = list(group_list(total_label[2],10))
+    nn1_groups_data.pop()
+    nn1_groups_label.pop()
+    nn2_groups_data.pop()
+    nn2_groups_label.pop()
+    nn3_groups_data.pop()
+    nn3_groups_label.pop()
+    #print "length of site one group data " + str(len(nn1_groups_data))
+    #nn2_groups_data = total_data[len(total_data)/2:]
+    #nn1_groups_label = total_label[:len(total_data)/2+1]
+    #nn2_groups_label = total_label[len(total_data)/2:]
+    
+    nets = list()  #Our differential networks
+    batches = list() #a list to store every separate site set
+    #Lists for our error to be plotted later
+        
+    nat = []
+    for i in range((min(len(nn1_groups_data),len(nn2_groups_data),len(nn2_groups_data)))):#
+	
+        print len(nn1_groups_data)-1
+        groups_data = list()
+        groups_label = list()
+        nets = list()
+        batches = list()
+        nnClassic1 = nnS.nn_build(1,[2,6,6,1],eta=eta,nonlin=nonlin)
+        nnClassic2 = nnS.nn_build(1,[2,6,6,1],eta=eta,nonlin=nonlin)
+        
+        for x in range(number_of_nets):
+            nets.append(nnDif.nn_build(1,[2,6,6,1],eta=eta,nonlin=nonlin))
+        
+        #Build the two site data sets
+        groups_data.append(np.asarray([item for sublist in nn1_groups_data[:i+1] for item in sublist]))
+        groups_data.append(np.asarray([item for sublist in nn2_groups_data[:i+1] for item in sublist]))
+        groups_data.append(np.asarray([item for sublist in nn3_groups_data[:i+1] for item in sublist]))
+        print "groups data length " + str(len(nn1_groups_label))
+        print "groups data length " + str(len(nn1_groups_data[i]))
+        print "groups data length " + str(len(nn2_groups_label[i]))
+        print "groups data length " + str(len(nn2_groups_data))
+
+        groups_label.append(np.asarray([item for sublist in nn1_groups_label[:i+1] for item in sublist]))
+        groups_label.append(np.asarray([item for sublist in nn2_groups_label[:i+1] for item in sublist]))
+        groups_label.append(np.asarray([item for sublist in nn3_groups_label[:i+1] for item in sublist]))
+        total_groups_data = np.asarray([item for sublist in groups_data for item in sublist])
+        total_groups_label =  np.asarray([item for sublist in groups_label for item in sublist])
+        #Our classic combined nn    
+        nnClassic3 = nnS.nn_build(1,[2,6,6,1],eta=eta,nonlin=nonlin)
+        nnClassic4 = nnS.nn_build(1,[2,6,6,1],eta=eta,nonlin=nonlin)
+        
+        for s in range(number_of_nets):
+            batches.append([x for x in iter_minibatches(2,groups_data[s].T, groups_label[s])])
+        t = [x for x in iter_minibatches(2,groups_data[0].T, groups_label[0])]
+        t2 = [x for x in iter_minibatches(2,groups_data[1].T, groups_label[1])]    
+        t3 = [x for x in iter_minibatches(2,groups_data[2].T, groups_label[2])]
+        t4 = [x for x in iter_minibatches(2,total_groups_data.T, total_groups_label)]
+        err = []
+        #Run the batches through the algos
+        iters = 20000
+        visitClassicBatches(nnClassic1,t, it=iters)
+        visitClassicBatches(nnClassic2,t2, it=iters)
+        visitClassicBatches(nnClassic3,t3, it=iters)
+        visitClassicBatches(nnClassic4,t4, it=iters)
+        visitbatches(nets, batches, err, it=iters)
+        
+        #calculate error
+        classic = accuracyClassic(nnClassic1,validation_data,validation_label, thr=0.5)
+        one = accuracy(nets[0], validation_data, validation_label, thr=0.5)
+        classic2 = accuracyClassic(nnClassic2,validation_data,validation_label, thr=0.5)
+        classic3 = accuracyClassic(nnClassic3,validation_data,validation_label, thr=0.5)
+        classic4 = accuracyClassic(nnClassic4,validation_data,validation_label, thr=0.5)
+        nat = nets[0]
+        #build plottable arrays
+        nn1Acc[te][i] = one
+        classAcc1[te][i] = classic
+        classAcc2[te][i] = classic2
+        classAcc3[te][i] = classic3
+        classAcc4[te][i] = classic4
+
+        #print "us " + str(one) + " c1 " + str(classic) + " c2 " + str(classic2) + " cc " + str(classic3)
+
+#nn1Acc[:] = [x / 10 for x in nn1Acc]
+#classAcc1[:] = [x / 10 for x in classAcc1]
+#classAcc2[:] = [x / 10 for x in classAcc2]
+#classAcc3[:] = [x / 10 for x in classAcc3]
+np.savetxt("nn1Acc-lowiters-gaussProb.txt",nn1Acc)
+np.savetxt("classAcc1-lowiters-gaussProb.txt",classAcc1)
+np.savetxt("classAcc2-lowiters-gaussProb.txt",classAcc2)
+np.savetxt("classAcc3-lowiters-gaussProb.txt",classAcc3)
+np.savetxt("classAcc-lowiters-gaussProb.txt",classAcc4)
+#plt.xlabel("Number of batches [of size 50]")
+#plt.ylabel("Error rate")
+
+#plt.plot(nn1Acc[0], "-r", label = "differential")
+#plt.plot(nn1Acc[1], "-r", label = "differential")
+#plt.plot(classAcc2, "-b", label = "classic 1")
+#plt.plot(classAcc1, "-g", label = "classic 2")
+#plt.plot(classAcc3, "-p", label = "Classic Combined")
+#plt.legend(loc='upper right')
+
