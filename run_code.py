f6e83303fb1c50d5cf10f275b8ed6765032a9859
Noah Lewis <lhd231@gmail.com>
2016-06-10 10:27:17 -0600

    Clean up and comments

--- run_code.py
+++ run_code.py
@@ -1,5 +1,4 @@
 import numpy as np
-#from nn import nn_build, forward, master_node, plot_decision2D
 import nn as nnDif
 from sklearn.cross_validation import train_test_split
 from sklearn.datasets import make_moons
@@ -10,77 +9,52 @@ import nnS
 import math
 import random
 from scipy.stats import multivariate_normal as norm
+from random import shuffle
 
 minibatch = 2
 nonlin = 'relu'
 eta = 0.0025
 
 def gauss(x,mu,sigma):
-  
-#  y = 1 / (math.sqrt(2.0 * math.pi * sigma) * (math.exp(((x[0] - mu[0] + 0.0) / sigma **2) + ((x[1] - mu[1] + 0.0) / sigma **2))))
   return norm(mu,[[sigma,0],[0, sigma]]).pdf(x)
 
 
 def split(data, label):
-  #plt.scatter(data[:,0],data[:,1], c=label)
-  
-  print np.var(data)
-  pointMid1 = (0,1)
-  pointMid2 = (1,-.5)
-  pointEdge1 = (-.9,.4)
-  pointEdge2 = (0,.4)
-  pointEdge3 = (1,.5)
-  pointEdge4 = (2,.4)
-  brown1 = list()
-  brown2 = list()
-  green1 = list()
-  green2 = list()
-  red1 = list()
-  red2 = list()
-  brown1Label = list()
-  brown2Label = list()
-  green1Label = list()
-  green2Label = list()
-  red1Label = list()
-  red2Label = list()
-  max = 0
-  i = 0
-  for x,y in zip(data,label):
-    b1 = gauss(x,pointMid1,.1)
-    b2 = gauss(x,pointMid2,.1)
-    g1 = gauss(x,pointEdge1,.1)
-    g2 = gauss(x,pointEdge2,.1)
-    r1 = gauss(x,pointEdge3,.1)
-    r2 = gauss(x,pointEdge4,.1)
-    a = random.random()
-    if y==0 and a < g1:
-      green1.append(x)
-      green1Label.append(y)
-    elif y==1 and a < g2:
-      green2.append(x)
-      green2Label.append(y)
-    elif y==0 and a< b1:
-      brown1.append(x)
-      brown1Label.append(y)
-    elif y==1 and a < b2:
-      brown2.append(x)
-      brown2Label.append(y)
+  midPoints = [0] * 6
+  midPoints[0] = (0,1)
+  midPoints[1] = (1,-.5)
+  midPoints[2] = (-.9,.4)
+  midPoints[3] = (0,.4)
+  midPoints[4] = (1,.5)
+  midPoints[5] = (2,.4)
+  sectionsData = [[] for i in range(6)]
+  sectionsLabel = [[] for i in range(6)]
 
-    elif y==0 and a < r1:
-      red1.append(x)
-      red1Label.append(y)
-    elif y==1 and a < r2:
-      red2.append(x)
-      red2Label.append(y)
-    i += 1
+  for x,y in zip(data,label):
+    gaussResults = [[] for i in range(6)]
+    for i in range(6):
+      gaussResults[i] = gauss(x,midPoints[i],.1)
+
+    a = random.random()
+    s = 0
+    for i in range(6):
+      if y==i%2 and a < gaussResults[i]:
+	sectionsData[i].append(x)
+	sectionsLabel[i].append(s)
+      if i % 2 == 1:
+	s += 1
+  
   return_list_data = list()
   return_list_label = list()
-  return_list_data.append(brown1 + brown2) 
-  return_list_data.append(green1 + green2) 
-  return_list_data.append(red1 + red2)
-  return_list_label.append(brown1Label + brown2Label) 
-  return_list_label.append(green1Label + green2Label)
-  return_list_label.append(red1Label + red2Label)
+  for i in range(3):
+    random.seed(i)
+    arr = sectionsData[i*2] + sectionsData[i*2 + 1]
+    shuffle(arr)
+    return_list_data.append(arr)
+    random.seed(i)
+    arr = sectionsLabel[i*2] + sectionsLabel[i*2 + 1]
+    shuffle(arr)
+    return_list_label.append(arr)
   return return_list_data, return_list_label
 
 
@@ -95,23 +69,27 @@ def iter_minibatches(chunksize, data, labels):
         yield X_chunk, y_chunk
         chunkstartmarker += chunksize
 
-
+#Calls the master node per differentiated NN.  The master node runs the forward propogation
+#Then averages the returned weights and runs the back prop with the new average
 def visitbatches(nn, batches, errlist, it=1000):
     for c in range(it):
         for i in range(len(batches)):
             batch = batches[i]
             cc = np.mod(c,len(batch))
             nnDif.master_node(nn, batch[cc][0], batch[cc][1])
-            #err.append(r)
-
+     
+#calls the forward and back propogation for a classic NN
 def visitClassicBatches(nn,data, it=1000):
     for c in range(it):
         cc = np.mod(c,len(data));
         nnS.minibatch_fit(nn, data[cc][0], data[cc][1])
 
+#Calculate the error of an differentiated NN and return it as a percent
 def accuracy(nn, data, label, thr = 0.5):
     predict  = [ np.int8(nnDif.forward(nn,data[c,:]) > thr) == label[c] for c in range(data.shape[0])]
     return 100 * np.double(len(np.where(np.asarray(predict)==False)[0]))/np.double(len(predict))
+  
+#Calculate the error a classic NN and return as percent
 def accuracyClassic(nn, data, label, thr = 0.5):
     predict  = [ np.int8(nnS.forward(nn,data[c,:]) > thr) == label[c] for c in range(data.shape[0])]
     return 100 * np.double(len(np.where(np.asarray(predict)==False)[0]))/np.double(len(predict))
@@ -128,19 +106,20 @@ print len(nn1Acc)
 print "len"
 print len(nn1Acc[0])   
 number_of_nets = 3
+
+#Run the neural nets 10 times so we can find a more accurate curve
 for te in range(10):
     print te
     data, label = make_moons(n_samples=1500, shuffle=True, noise=0.2,random_state = int(time.time()))
     print data[0]
     data,validation_data,label,validation_label = train_test_split(data,label,train_size = .32)
-        #separate the data set into buckets
+    
+    #separate the data set into buckets
     total_data, total_label = split(data,label)
 
-     
-    #total_data = list(group_list(data,10))
-    #total_label = list(group_list(label,10))
-    #The two separate site sets
-    #nn1_groups_data = total_data[:len(total_data)/2+1]
+    
+    #These are the lists of data per site.  The list contains lists of 10 data midPoints
+    #We pop off the last one because it most likely does not have 10 items
     print len(total_data[0])
     print len(total_data[1])
     nn1_groups_data = list(group_list(total_data[0],10))
@@ -155,16 +134,11 @@ for te in range(10):
     nn2_groups_label.pop()
     nn3_groups_data.pop()
     nn3_groups_label.pop()
-    #print "length of site one group data " + str(len(nn1_groups_data))
-    #nn2_groups_data = total_data[len(total_data)/2:]
-    #nn1_groups_label = total_label[:len(total_data)/2+1]
-    #nn2_groups_label = total_label[len(total_data)/2:]
+
     
     nets = list()  #Our differential networks
     batches = list() #a list to store every separate site set
-    #Lists for our error to be plotted later
-        
-    nat = []
+
     for i in range((min(len(nn1_groups_data),len(nn2_groups_data),len(nn2_groups_data)))):#
 	
         print len(nn1_groups_data)-1
@@ -217,7 +191,7 @@ for te in range(10):
         classic2 = accuracyClassic(nnClassic2,validation_data,validation_label, thr=0.5)
         classic3 = accuracyClassic(nnClassic3,validation_data,validation_label, thr=0.5)
         classic4 = accuracyClassic(nnClassic4,validation_data,validation_label, thr=0.5)
-        nat = nets[0]
+
         #build plottable arrays
         nn1Acc[te][i] = one
         classAcc1[te][i] = classic
@@ -225,24 +199,11 @@ for te in range(10):
         classAcc3[te][i] = classic3
         classAcc4[te][i] = classic4
 
-        #print "us " + str(one) + " c1 " + str(classic) + " c2 " + str(classic2) + " cc " + str(classic3)
 
-#nn1Acc[:] = [x / 10 for x in nn1Acc]
-#classAcc1[:] = [x / 10 for x in classAcc1]
-#classAcc2[:] = [x / 10 for x in classAcc2]
-#classAcc3[:] = [x / 10 for x in classAcc3]
 np.savetxt("nn1Acc-lowiters-gaussProb.txt",nn1Acc)
 np.savetxt("classAcc1-lowiters-gaussProb.txt",classAcc1)
 np.savetxt("classAcc2-lowiters-gaussProb.txt",classAcc2)
 np.savetxt("classAcc3-lowiters-gaussProb.txt",classAcc3)
 np.savetxt("classAcc-lowiters-gaussProb.txt",classAcc4)
-#plt.xlabel("Number of batches [of size 50]")
-#plt.ylabel("Error rate")
 
-#plt.plot(nn1Acc[0], "-r", label = "differential")
-#plt.plot(nn1Acc[1], "-r", label = "differential")
-#plt.plot(classAcc2, "-b", label = "classic 1")
-#plt.plot(classAcc1, "-g", label = "classic 2")
-#plt.plot(classAcc3, "-p", label = "Classic Combined")
-#plt.legend(loc='upper right')
 
